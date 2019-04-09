41ba09fd51a2e482bcca7bb49b2250f65d7af20e
unknown <rahulguptaxyz15@gmail.com>
2019-04-10 01:08:27 +0530

    UI all pages simple CSS

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor() { }
 
